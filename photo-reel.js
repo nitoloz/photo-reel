08066cda1fd605da3a528dd0ba6e74df30700e30
Andrey Zolotin <andrey.zolotin@gmail.com>
2018-12-05 13:10:00 +0100

    feat: add width,height and transition parameters to photo reel

--- photo-reel.js
+++ photo-reel.js
@@ -1,7 +1,15 @@
 function d3Reel() {
+
+    let width = null,
+        height = null,
+        transition = null;
+
     function chart(selection) {
         selection.each(function (dataset) {
             const divBounds = selection[0][0].getBoundingClientRect();
+
+            height = height || divBounds.height;
+            width = width || divBounds.width;
 
             const disortionScale = d3.scale.linear()
                 .domain([0, 100])
@@ -9,14 +17,14 @@ function d3Reel() {
 
             const x = d3.fisheye.ordinal()
                 .domain(d3.range(dataset.length))
-                .rangeRoundBands([0, divBounds.width])
-                .focus(divBounds.width / 2)
+                .rangeRoundBands([0, width])
+                .focus(width / 2)
                 .distortion(disortionScale(dataset.length));
 
             const svg = selection.append("svg")
                 .attr('id', '#reel-svg')
-                .attr("width", divBounds.width)
-                .attr("height", divBounds.height)
+                .attr("width", width)
+                .attr("height", height)
                 .style('cursor', 'pointer')
                 .style('-webkit-box-reflect', 'below 0px -webkit-linear-gradient(bottom, ' +
                     'rgba(255, 255, 255, 0.3) 0%, transparent 40%, transparent 100%)');
@@ -26,13 +34,9 @@ function d3Reel() {
                 .selectAll('image')
                 .data(dataset);
 
-            const groupsEnter = groups
+            const imageEnter = groups
                 .enter()
-                .append('image');
-
-            groups.exit().remove();
-
-            const imageEnter = groupsEnter
+                .append('image')
                 .attr("xlink:href", function (d, i) {
                     return d.imageUrl;
                 })
@@ -41,13 +45,18 @@ function d3Reel() {
                     return x(i);
                 })
                 .attr("width", function (d, i) {
-                    return Math.min(x.rangeBand(i), divBounds.height);
+                    return Math.min(x.rangeBand(i), height);
                 })
                 .attr("height", function (d, i) {
-                    return Math.min(x.rangeBand(i), divBounds.height);
+                    return Math.min(x.rangeBand(i), height);
+                })
+                .style('transition', function () {
+                    return transition ? `all ${transition}s` : null;
                 });
 
-            groupsEnter
+            groups.exit().remove();
+
+            imageEnter
                 .on("mousemove", function () {
                     onMouseEnter(d3.mouse(this)[0]);
                 });
@@ -59,14 +68,32 @@ function d3Reel() {
                         return x(i);
                     })
                     .attr("width", function (d, i) {
-                        return Math.min(x.rangeBand(i), divBounds.height);
+                        return Math.min(x.rangeBand(i), height);
                     })
                     .attr("height", function (d, i) {
-                        return Math.min(x.rangeBand(i), divBounds.height);
+                        return Math.min(x.rangeBand(i), height);
                     });
             }
         });
     }
+
+    chart.width = function (value) {
+        if (!arguments.length) return width;
+        width = value;
+        return chart;
+    };
+
+    chart.height = function (value) {
+        if (!arguments.length) return height;
+        height = value;
+        return chart;
+    };
+
+    chart.transition = function (value) {
+        if (!arguments.length) return transition;
+        transition = value;
+        return chart;
+    };
 
     return chart;
 }
